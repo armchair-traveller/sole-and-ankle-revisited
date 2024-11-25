13d873f5312cc5c3e5bef4625f80dd09d96762c6
Armchair Traveller <sentient.sober.armchair@gmail.com>
2024-11-25 13:18:42 -0800

    ex1: setup breakpoints

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from 'react'
+import styled from 'styled-components/macro'
+import { ThemeProvider } from 'styled-components'
 
-import Header from '../Header';
-import ShoeIndex from '../ShoeIndex';
+import Header from '../Header'
+import ShoeIndex from '../ShoeIndex'
+const theme = {
+  breakpoints: {
+    phone: '37.5rem', // 600px
+    tablet: '59.375rem', // 950px
+    laptop: '81.25rem', // 1300px
+  },
+  // * example: @media (max-width: ${({ theme }) => theme.breakpoints.tablet})
+}
 
 const App = () => {
-  const [sortId, setSortId] = React.useState('newest');
+  const [sortId, setSortId] = React.useState('newest')
 
   return (
     <>
-      <Header />
-      <Main>
-        <ShoeIndex sortId={sortId} setSortId={setSortId} />
-      </Main>
+      <ThemeProvider theme={theme}>
+        <Header />
+        <Main>
+          <ShoeIndex sortId={sortId} setSortId={setSortId} />
+        </Main>
+      </ThemeProvider>
     </>
-  );
-};
+  )
+}
 
 const Main = styled.main`
   padding: 64px 32px;
-`;
+`
 
-export default App;
+export default App
