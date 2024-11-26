25b7f27718c1afc19febb3eaf4da0fb16269f257
Armchair Traveller <sentient.sober.armchair@gmail.com>
2024-11-25 22:12:39 -0800

    Refactor Header component layout and enhance mobile actions
    - nothing much to learn here so just going with the solution. If there was one thing, it's that you can just hide the superheader and add padding-top to another header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from 'react'
+import styled from 'styled-components/macro'
 
-import { COLORS, WEIGHTS } from '../../constants';
-import Logo from '../Logo';
-import SuperHeader from '../SuperHeader';
-import MobileMenu from '../MobileMenu';
+import { COLORS, WEIGHTS } from '../../constants'
+import Logo from '../Logo'
+import Icon from '../Icon'
+import UnstyledButton from '../UnstyledButton'
+import SuperHeader from '../SuperHeader'
+import MobileMenu from '../MobileMenu'
+import VisuallyHidden from '../VisuallyHidden'
 
 const Header = () => {
-  const [showMobileMenu, setShowMobileMenu] = React.useState(false);
+  const [showMobileMenu, setShowMobileMenu] = React.useState(false)
 
   // For our mobile hamburger menu, we'll want to use a button
   // with an onClick handler, something like this:
@@ -18,27 +21,38 @@ const Header = () => {
     <header>
       <SuperHeader />
       <MainHeader>
-        <Side>
+        <LogoWrapper>
           <Logo />
-        </Side>
-        <Nav>
+        </LogoWrapper>
+        <DesktopNav>
           <NavLink href="/sale">Sale</NavLink>
           <NavLink href="/new">New&nbsp;Releases</NavLink>
           <NavLink href="/men">Men</NavLink>
           <NavLink href="/women">Women</NavLink>
           <NavLink href="/kids">Kids</NavLink>
           <NavLink href="/collections">Collections</NavLink>
-        </Nav>
-        <Side />
+        </DesktopNav>
+        <MobileActions>
+          <ShoppingBagButton>
+            <Icon id="shopping-bag" />
+            <VisuallyHidden>Open cart</VisuallyHidden>
+          </ShoppingBagButton>
+          <UnstyledButton>
+            <Icon id="search" />
+            <VisuallyHidden>Search</VisuallyHidden>
+          </UnstyledButton>
+          <UnstyledButton>
+            <Icon id="menu" />
+            <VisuallyHidden>Open menu</VisuallyHidden>
+          </UnstyledButton>
+        </MobileActions>
+        <Filler />
       </MainHeader>
 
-      <MobileMenu
-        isOpen={showMobileMenu}
-        onDismiss={() => setShowMobileMenu(false)}
-      />
+      <MobileMenu isOpen={showMobileMenu} onDismiss={() => setShowMobileMenu(false)} />
     </header>
-  );
-};
+  )
+}
 
 const MainHeader = styled.div`
   display: flex;
@@ -46,17 +60,61 @@ const MainHeader = styled.div`
   padding: 18px 32px;
   height: 72px;
   border-bottom: 1px solid ${COLORS.gray[300]};
-`;
 
-const Nav = styled.nav`
+  ${({ theme }) => theme.media.tablet} {
+    justify-content: space-between;
+    align-items: center;
+    border-top: 4px solid ${COLORS.gray[900]};
+  }
+
+  ${({ theme }) => theme.media.phone} {
+    padding-left: 16px;
+    padding-right: 16px;
+  }
+`
+
+const DesktopNav = styled.nav`
   display: flex;
   gap: 48px;
   margin: 0px 48px;
-`;
 
-const Side = styled.div`
+  ${({ theme }) => theme.media.tablet} {
+    display: none;
+  }
+`
+
+const MobileActions = styled.div`
+  display: none;
+
+  ${({ theme }) => theme.media.tablet} {
+    gap: 32px;
+    display: flex;
+  }
+
+  ${({ theme }) => theme.media.phone} {
+    gap: 16px;
+  }
+`
+
+const LogoWrapper = styled.div`
   flex: 1;
-`;
+
+  ${({ theme }) => theme.media.tablet} {
+    flex: revert;
+  }
+`
+
+const ShoppingBagButton = styled(UnstyledButton)`
+  transform: translateX(-2px);
+`
+
+const Filler = styled.div`
+  flex: 1;
+
+  ${({ theme }) => theme.media.tablet} {
+    display: none;
+  }
+`
 
 const NavLink = styled.a`
   font-size: 1.125rem;
@@ -68,6 +126,6 @@ const NavLink = styled.a`
   &:first-of-type {
     color: ${COLORS.secondary};
   }
-`;
+`
 
-export default Header;
+export default Header
