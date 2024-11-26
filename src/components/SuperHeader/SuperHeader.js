2083e479c31a6d75b5848c975fb9aaa44dffe499
Armchair Traveller <sentient.sober.armchair@gmail.com>
2024-11-25 21:53:08 -0800

    ex2 initial superheader

--- src/components/SuperHeader/SuperHeader.js
+++ src/components/SuperHeader/SuperHeader.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from 'react'
+import styled from 'styled-components/macro'
 
-import { COLORS } from '../../constants';
+import { COLORS } from '../../constants'
 
-import SearchInput from '../SearchInput';
-import UnstyledButton from '../UnstyledButton';
-import Icon from '../Icon';
+import SearchInput from '../SearchInput'
+import UnstyledButton from '../UnstyledButton'
+import Icon from '../Icon'
 
 const SuperHeader = () => {
   return (
     <Wrapper>
-      <MarketingMessage>
-        Free shipping on domestic orders over $75!
-      </MarketingMessage>
+      <MarketingMessage>Free shipping on domestic orders over $75!</MarketingMessage>
       <SearchInput />
       <HelpLink href="/help">Help</HelpLink>
       <UnstyledButton>
         <Icon id="shopping-bag" strokeWidth={1} />
       </UnstyledButton>
     </Wrapper>
-  );
-};
+  )
+}
 
 const Wrapper = styled.div`
   display: flex;
@@ -32,12 +30,18 @@ const Wrapper = styled.div`
   height: 40px;
   padding-left: 32px;
   padding-right: 32px;
-`;
+  ${({ theme }) => theme.media.phone} {
+    > * {
+      display: none;
+    }
+    height: 0.5rem;
+  }
+`
 
 const MarketingMessage = styled.span`
   color: ${COLORS.white};
   margin-right: auto;
-`;
+`
 
 const HelpLink = styled.a`
   color: inherit;
@@ -47,6 +51,6 @@ const HelpLink = styled.a`
   &:not(:focus-visible) {
     outline: none;
   }
-`;
+`
 
-export default SuperHeader;
+export default SuperHeader
